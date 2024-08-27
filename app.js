0cbc0f5917a89768f4ec28ae506c94ede78419cb
haem <eriramoreno@users.noreply.github.com>
2024-08-26 19:20:07 -0500

    funciones

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-
+function mostarMensajeEnLaConsola() {
+    console.log('El botón fue clicado!')
+}
+function mostrarAlerta() {
+    let ciudad = prompt("Por favor, ingresa el nombre de una ciudad de Brasil:");
+    alert("Estuve en " + ciudad + " y me acordé de ti.");
+}
+function sumaDosNumeros(){
+    let primerNumero = parseInt(prompt('Digite el primer numero'));
+    let segunNumero = parseInt(prompt('Digite el segundo numero'));
+    let resultado = primerNumero + segunNumero;
+    alert('${primerNumero} + ${segunNumero} = ${resultado}')
+}
+function mostarAlerta(){
+    alert("AMO JS");
+} 
